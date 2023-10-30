cf3e4d12ac42562fb697ad041a84387c2c7f84c1
bilal-inksmith <109742973+mbakhtar@users.noreply.github.com>
2023-10-30 09:04:28 -0400

    positional servo angle change

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
         fwdMotors.drive(fwdMotors.DrivingDirection.Forward, 100)
         basic.pause(2000)
-        fwdMotors.middleServo.fwdSetAngle(45)
-        basic.pause(500)
         fwdMotors.middleServo.fwdSetAngle(0)
+        basic.pause(500)
+        fwdMotors.middleServo.fwdSetAngle(45)
     }
     fwdMotors.stop()
 })
@@ -13,4 +13,4 @@ fwdMotors.leftServo,
 fwdMotors.rightServo,
 0
 )
-fwdMotors.middleServo.fwdSetAngle(0)
+fwdMotors.middleServo.fwdSetAngle(45)
