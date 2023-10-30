a392372d279a844af8d9616dd96e2f69756e755e
bilal-inksmith <109742973+mbakhtar@users.noreply.github.com>
2023-10-30 09:02:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        fwdMotors.drive(fwdMotors.DrivingDirection.Forward, 100)
+        basic.pause(2000)
+        fwdMotors.middleServo.fwdSetAngle(45)
+        basic.pause(500)
+        fwdMotors.middleServo.fwdSetAngle(0)
+    }
+    fwdMotors.stop()
+})
+fwdMotors.setupDriving(
+fwdMotors.leftServo,
+fwdMotors.rightServo,
+0
+)
+fwdMotors.middleServo.fwdSetAngle(0)
